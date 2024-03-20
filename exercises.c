69f02734bff5a0bd2b78a0828f5fb286c289b477
Your Name <you@example.com>
2024-03-20 20:29:30 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -85,7 +85,7 @@ void eliminaElementos(List *L, int elem) {
   ent = first(L);
   while(ent != NULL){
     if(*ent == elem){
-      popCurrent(L,elem);        
+      popCurrent(L,*ent);        
     }
   }
 
